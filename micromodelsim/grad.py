4e7a4dfc762ed1d851d934d27f9c043d33fb3203
Leevi <leevi.kerkela@protonmail.com>
2023-01-23 14:43:34 +0000

    Improved module and tests

--- micromodelsim/grad.py
+++ micromodelsim/grad.py
@@ -1,11 +1,6 @@
-try: 
-    import JAX.numpy as np
-except ImportError:
-    print("Unable to import JAX, numpy is imported instead")
-    import numpy as np
+import numpy as np
 
-
-from .sh import sh, l_max, n_coeffs
+from .sh import sh
 
 
 def _vec2vec_rotmat(v, k):
@@ -13,20 +8,20 @@ def _vec2vec_rotmat(v, k):
 
     Parameters
     -----------
-    v : array_like
+    v : numpy.ndarray
         1D array with length 3.
-    k : array_like
+    k : numpy.ndarray
         1D array with length 3.
 
     Returns
     ---------
-    R : numpy.ndarray
-        3 by 3 rotation matrix.
+    numpy.ndarray
+        Rotation matrix.
     """
     v = v / np.linalg.norm(v)
     k = k / np.linalg.norm(k)
     axis = np.cross(v, k)
-    if np.linalg.norm(axis) < 1e-10:
+    if np.linalg.norm(axis) < 1e-10:  # the vectors are aligned
         if np.linalg.norm(v - k) > np.linalg.norm(v):
             return -np.eye(3)
         else:
@@ -47,12 +42,13 @@ class Gradient(object):
 
     Parameters
     ----------
-    bvals : array_like
-        b-values in an array with shape (number of measurements,).
-    bvecs : array_like
-        b-vectors in an array with shape (number of measurements, 3).
+    bvals : numpy.ndarray
+        b-values in a 1D array whose length is equal to the number of
+        measurements.
+    bvecs : numpy.ndarray
+        b-vectors in a 2D array with shape (number of measurements, 3).
     bten_shape : {"linear", "planar", "spherical"}, optional
-        b-tensor shape.
+        b-tensor shape. The default linear.
 
     Attributes
     ----------
@@ -60,49 +56,47 @@ class Gradient(object):
         b-values.
     bvecs : numpy.ndarray
         b-vectors.
+    bs : numpy.ndarray
+        Unique b-values.
     bten_shape : numpy.ndarray
         b-tensor shape.
     btens : numpy.ndarray
         b-tensors.
-    bs : int
-        Unique b-values.
     shell_idx_list : list
         Indices of `bvals` and `bvecs` corresponding to different shells.
     """
 
     def __init__(self, bvals, bvecs, bten_shape="linear"):
 
+        if not isinstance(bvals, np.ndarray):
+            raise TypeError("Incorrect type for `bvals`")
         if bvals.ndim != 1:
-            raise ValueError("Incorrect value for `bvals`")
+            raise ValueError("Incorrect shape for `bvals`")
+        if not isinstance(bvecs, np.ndarray):
+            raise TypeError("Incorrect type for `bvecs`")
         if bvecs.ndim != 2 or bvecs.shape[1] != 3:
-            raise ValueError(f"Incorrect value for `bvecs`")
+            raise ValueError("Incorrect shape for `bvecs`")
         if len(bvals) != len(bvecs):
-            raise ValueError("`bvals` and `bvecs` should be the same length.")
+            raise ValueError("`bvals` and `bvecs` must have the same length")
 
         self.bvals = bvals
         self.bvecs = bvecs
         self.bten_shape = bten_shape
         self.btens = np.zeros((len(self.bvals), 3, 3))
         for i, (bval, bvec) in enumerate(zip(self.bvals, self.bvecs)):
-            R = _vec2vec_rotmat(np.array([1, 0, 0]), bvec)
-            if self.bten_shape == "linear":
-                self.btens[i] = (
-                    R @ np.array([[1, 0, 0], [0, 0, 0], [0, 0, 0]]) @ R.T * bval
-                )
-            elif self.bten_shape == "planar":
-                self.btens[i] = (
-                    R @ np.array([[0, 0, 0], [0, 1, 0], [0, 0, 1]]) / 2 @ R.T * bval
-                )
-            elif self.bten_shape == "spherical":
-                self.btens[i] = bval * np.eye(3) / 3
+            if bval == 0 and np.all(bvec == np.zeros(3)):
+                self.btens[i] = np.zeros((3, 3))
+            else:
+                R = _vec2vec_rotmat(np.array([1, 0, 0]), bvec)
+                if self.bten_shape == "linear":
+                    self.btens[i] = (
+                        R @ np.array([[1, 0, 0], [0, 0, 0], [0, 0, 0]]) @ R.T * bval
+                    )
+                elif self.bten_shape == "planar":
+                    self.btens[i] = (
+                        R @ np.array([[0, 0, 0], [0, 1, 0], [0, 0, 1]]) / 2 @ R.T * bval
+                    )
+                elif self.bten_shape == "spherical":
+                    self.btens[i] = bval * np.eye(3) / 3
         self.bs = np.unique(bvals)
         self.shell_idx_list = [np.where(self.bvals == b)[0] for b in self.bs]
-        self._bvecs_isft_list = []
-        for idx in self.shell_idx_list:
-            thetas = np.arccos(self.bvecs[idx, 2])
-            phis = np.arctan2(self.bvecs[idx, 1], self.bvecs[idx, 0]) + np.pi
-            bvecs_isft = np.zeros((len(self.bvecs[idx]), n_coeffs))
-            for l in range(0, l_max + 1, 2):
-                for m in range(-l, l + 1):
-                    bvecs_isft[:, int(0.5 * l * (l + 1) + m)] = sh(l, m, thetas, phis)
-            self._bvecs_isft_list.append(bvecs_isft)
--- micromodelsim/tests/test_grad.py
+++ micromodelsim/tests/test_grad.py
@@ -1,5 +1,5 @@
 import numpy as np
-import numpy.testing as npt 
+import numpy.testing as npt
 
 import micromodelsim as mmsim
 
@@ -18,6 +18,36 @@ def test__vec2vec_rotmat():
 
 
 def test_Gradient():
-    npt.assert_raises(ValueError, mmsim.Gradient, bvals=np.zeros(3), bvecs=np.zeros((3, 2)))
-    npt.assert_raises(ValueError, mmsim.Gradient, bvals=np.zeros((3, 3)), bvecs=np.zeros((3, 3)))
-    npt.assert_raises(ValueError, mmsim.Gradient, bvals=np.zeros(3), bvecs=np.zeros((2, 3)))
+    npt.assert_raises(TypeError, mmsim.Gradient, bvals=[1, 2], bvecs=np.zeros((2, 3)))
+    npt.assert_raises(
+        ValueError, mmsim.Gradient, bvals=np.zeros((2, 3)), bvecs=np.zeros((2, 3))
+    )
+    npt.assert_raises(TypeError, mmsim.Gradient, bvals=np.zeros(2), bvecs=[1, 2])
+    npt.assert_raises(
+        ValueError, mmsim.Gradient, bvals=np.zeros(2), bvecs=np.zeros((3, 3))
+    )
+    npt.assert_raises(
+        ValueError, mmsim.Gradient, bvals=np.zeros(3), bvecs=np.zeros((2, 3))
+    )
+    bvals = np.array([1, 2, 2])
+    bvecs = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    gradient = mmsim.Gradient(bvals, bvecs)
+    assert np.all(gradient.bvals == bvals)
+    assert np.all(gradient.bvecs == bvecs)
+    assert np.all(gradient.bs == np.unique(bvals))
+    assert gradient.bten_shape == "linear"
+    assert np.all(
+        (
+            gradient.btens
+            - np.array(
+                [
+                    [[1, 0, 0], [0, 0, 0], [0, 0, 0]],
+                    [[0, 0, 0], [0, 2, 0], [0, 0, 0]],
+                    [[0, 0, 0], [0, 0, 0], [0, 0, 2]],
+                ]
+            )
+        )
+        < 1e-10,
+    )
+    assert np.all(gradient.shell_idx_list[0] == np.array([0]))
+    assert np.all(gradient.shell_idx_list[1] == np.array([1, 2]))
